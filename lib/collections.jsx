561857533a069565557a27d76be39573ee796ca4
tebenia <tebe.deye@gmail.com>
2016-07-14 15:59:53 +0700

    9. Added Simple Collection using collection2;
    Adding a collections, Allow, Deny, and create a schema model.

---
+++ lib/collections.jsx
@@ -0,0 +1,29 @@
+import Mongo from "meteor/mongo";
+
+export const Books = new Mongo.Collection("books");
+
+Books.allow({
+  insert() { return false; },
+  update() { return false; },
+  remove() { return false; }
+});
+
+Books.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; }
+});
+
+const BooksSchema = new SimpleSchema({
+	title {
+		type: String,
+		label: "Title"
+	},
+	createdAt: {
+		type: Date,
+		label: 'The date this checkin was created',
+		autoValue(){
+			return new Date();
+		}
+	}
+});
