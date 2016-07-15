0cc28a5765c120bd3ab5d25a2533538a8640c043
tebenia <tebe.deye@gmail.com>
2016-07-15 09:04:02 +0700

    12. Successfully show a template with Material-UI
    using normalise.css to normalise any element tag.
    add a child component using IndexRoute inside MainLayout.
    using blaze component to make a login button using github OAuth.
    using Material-UI AppBar as header bar.

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import {Router, Route, browserHistory} from "react-router";
+import {Router, Route, IndexRoute, browserHistory} from "react-router";
 import {Index} from "../../ui/components/index";
+import {MainLayout} from "../../ui/layouts/main";
 
 export const Routes = () => (
 	<Router history = {browserHistory}>
-		<Route path="/" component={Index}/>
+		<Route path="/" component={MainLayout}>
+			<IndexRoute component={Index}/>
+		</Route>
 	</Router>
 );
---
+++ imports/ui/layouts/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
+import {AppBar} from "material-ui";
+import AccountsUIWrapper from "../components/accountsUIWrapper";
+
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
+
+export const MainLayout = ({children}) => (
+	<MuiThemeProvider muiTheme={lightMuiTheme}>
+		<div>
+			<AppBar 
+				title="Book Trading App"
+				iconElementRight={<AccountsUIWrapper/>}
+				zDepth={4}
+			/>
+			{children}
+		</div>
+	</MuiThemeProvider>
+);
+
+MainLayout.PropTypes = {
+	children: React.PropTypes.object
+}
